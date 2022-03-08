f2346213b507d40aa518680aa1fdf76111d7411e
Ruan Pablo <ruan_goio10@hotmail.com>
2022-03-08 15:26:10 -0300

    Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
 
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>ig.News</title>
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Início | ig.News</title>
+            </Head>
 
+            <main className={styles.contentContainer}>
+                <section className={styles.hero}>
+                    <span>👏 Hey Welcome</span>
+                    <h1>News about <br />the <span>React</span> world.</h1>
 
-    </>
-  )
+                    <p>
+                        get access to all the publications <br />
+                        <span>for $9.90 month</span>
+                    </p>
+                </section>
+
+                <img src="/images/avatar.svg" alt="Girl Coding" />
+            </main>
+        </>
+    )
 }
