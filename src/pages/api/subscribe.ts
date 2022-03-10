9483dc04cdc253e30872ae325604100bfa26697b
Ruan Pablo <ruan_goio10@hotmail.com>
2022-03-10 16:41:25 -0300

    Check duplicate custumer_id

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -2,18 +2,64 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { stripe } from './../../services/stripe';
 
+import { query as q } from "faunadb";
+import { fauna } from "../../services/fauna";
+
+
+
+type User = {
+    ref: {
+        id: string,
+    },
+    data: {
+        stripe_custumer_id: string,
+    }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
 
         const session = await getSession({ req });
 
-        const stripecustumer = await stripe.customers.create({
-            email: session.user.email,
-            // metadata
-        });
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        );
+
+
+        let custumerId = user.data.stripe_custumer_id;
+
+        if (!custumerId) {
+            const stripecustumer = await stripe.customers.create({
+                email: session.user.email,
+                // metadata
+            });
+
+
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_custumer_id: stripecustumer.id
+                        }
+                    }
+                )
+            );
+
+
+            custumerId = stripecustumer.id
+        }
+
+
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripecustumer.id,
+            customer: custumerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
